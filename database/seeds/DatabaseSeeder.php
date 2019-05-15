b0ef38f93bd137eec22aefe668d55899b2ff5765
ysiouda <ysiouda@erasme.org>
2019-05-15 11:51:45 +0200

    db seeder for monitoring

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(ThematicTableSeeder::class);
         $this->call(ItemTableSeeder::class);
+        $this->call(MachineTableSeeder::class);
     }
 }
