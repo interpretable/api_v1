3a6f594d701e4c723b6b52b0229eb01b6df2bf3f
ysiouda <ysiouda@erasme.org>
2019-07-30 08:22:01 +0200

    zones update

--- database/seeds/ItemTableSeeder.php
+++ database/seeds/ItemTableSeeder.php
@@ -197,7 +197,8 @@ class ItemTableSeeder extends Seeder
             'medias' => [
                 "default/SANTE/problemes_cutanes/zone1.png",
                 "default/SANTE/problemes_cutanes/zone2.png", 
-                "default/SANTE/problemes_cutanes/zone3.png"
+                "default/SANTE/problemes_cutanes/zone3.png",
+                "default/SANTE/problemes_cutanes/zone4.png"
             ],
             'card_id' => 0,
             'card_picture' => 'default/SANTE/problemes_cutanes/carte.jpg',
